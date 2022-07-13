1f6c08c995a0a79d753690a0cc073a145f1ce1a3
FW960 <artuninp@gmail.com>
2022-07-14 06:22:06 +0800

    Парсер сайта и запись в БД

---
+++ Project/Program.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Entities;
+using System.Threading;
+using PageInteraction;
+using Repo;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+        }
+        static void Init()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.lesegais.ru/open-area/deal");
+
+            Thread.Sleep(3000); // Время для того чтобы страница успела подгрузится.
+
+            WebSite website = new WebSite(driver);
+
+            Run(website);
+        }
+        static void Run(WebSite website)
+        {
+            bool started = true;
+
+            DbRepository repo = new DbRepository();
+
+            while (true)
+            {
+                if (started == true)
+                {
+                    started = false;
+                }
+                else
+                {
+                    Thread.Sleep(60000);
+                }
+
+                DealDTO[] deals = website.ReadPageData();
+
+                website.NextPageClick();
+
+                for (int i = 0; i < deals.Length; i++)
+                {
+                    repo.Create(deals[i]);
+                }
+
+            }
+
+        }
+    }
+}
